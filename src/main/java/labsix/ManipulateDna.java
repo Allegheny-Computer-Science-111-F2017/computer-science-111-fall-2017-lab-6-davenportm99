6e7feea8cb2d0b9df3cb599478d1a5e6888b47ab
Marcus Davenport <davenportm@aldenv166.allegheny.edu>
2017-10-12 16:33:25 -0400

    Material for lab 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,22 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This program is called to ManipulateDna.
+ *
+ */
 public class ManipulateDna {
+  /** This program is going to Manipulate Dna.
+   *
+   *
+   */
 
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Marcus Davenport " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +30,37 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dna = scanner.nextLine();
+    String complementDna = dna.toUpperCase();
+    System.out.println(" Okay I am going to manipulate the DNA string \"" + dna + "\"");
     // Step Two: Compute the complement of the DNA String
-
+    complementDna = complementDna.replace('A' , 't' ); 
+    complementDna = complementDna.replace('T' , 'a' );
+    complementDna = complementDna.replace('C' , 'g' );
+    complementDna = complementDna.replace('G' , 'c' );
+    System.out.println("Complement of " + dna.toUpperCase() + " is " + complementDna.toUpperCase());
+    dna = dna.toUpperCase();
+    
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    String allowed = ("ACTG ");
+    Random ran = new Random();
+    int randomNum = ran.nextInt(4);
+    int randomNum2 = ran.nextInt(3);
+    char randomletter = allowed.charAt(randomNum);
+    char randomletter2 = allowed.charAt(randomNum2);
+    System.out.println("Inserting " + randomletter 
+        + " at position 0 gives " + randomletter + dna.toUpperCase());
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    String first = dna.substring(0 , randomNum);
+    String second = dna.substring(randomNum + 1, dna.length());
+    System.out.println("Deleting from position " + randomNum + " gives " + first + second);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    String three = dna.substring(0 , randomNum);
+    String four = dna.substring(randomNum + 1, dna.length());
+    System.out.println("Changing position " + (randomNum + 1) + " gives " + first +  randomletter2
+        + second);
     // Step Six: Display a final thankyou message
-
+    System.out.println("Thanks for using the ManipulateDna program. Have an awesome day. " );
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,12 @@
 # Reflection by
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+today I have learned more in the class then I have ever learned. I finally,
+afte 3 weeks understand what a method is. I used the variable random number to
+change bioinformatics. I used the method length. I also used the method
+toUpperCase. I succesfully changed the DNA output.
+
+With the Help of my amazing professor I was succesfully able to manipulate dna
+ssequence. I now fully understand what a method is how to use it. I also know
+how to used diffrent classes to import multiple methods. I was able to hold a
+letter in the dna sequence until i could change it. I would like to say I'm
+getting better.
